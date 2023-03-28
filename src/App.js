6ef8342bbe3fe9ae3ebce5def2d43c5cff93ea46
saipepu <saipepu.mdy257@gmail.com>
2023-03-28 16:07:07 +0700

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './Pages/Login'
 import PageNotFound from './Pages/PageNotFound'
 import ScoringPage from './Pages/ScoringPage'
 import Leaderboard from './Pages/Leaderboard'
+import HistoryLog from './Pages/HistoryLog'
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/scoringPage/:id" element={<ScoringPage />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/history" element={<HistoryLog />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </HashRouter>
