04c0e6b0b24c9c1b13338f3faa6f263fdec7571a
saipepu <saipepu.mdy257@gmail.com>
2023-03-29 07:59:43 +0700

    changes

--- src/Pages/HistoryLog.jsx
+++ src/Pages/HistoryLog.jsx
@@ -14,9 +14,22 @@ const HistoryLog = () => {
   useEffect(() => {
     if(response?.success) {
       console.log(response)
-      let list = response.history.split(', ');
-      for(let i=0; i<list.length; i++) {}
-      setHistory(response.history.split(', '));
+      let list = response?.history?.split(',');
+      let list_1 = [];
+      for(let i=0; i<list.length; i++) {
+        if(i !== 0) {
+          let arr = list[i].split(" ")
+          let obj = {};
+          obj.teamName = arr[0];
+          obj.judgeName = arr[1];
+          obj.action = arr[2];
+          obj.fund = arr[3];
+          obj.time = arr[4]
+          list_1.push(obj);
+        }
+
+      }
+      setHistory(list_1);
     } else {
       setHistory([{teamName: 'Jelly bob', numberOfTransaction: 2, history: "ddi judge 1 plus 5000000,ddi judge 2 plus 500000"}])
     }
@@ -35,7 +48,7 @@ const HistoryLog = () => {
                 <p className={styles.numberOfTransaction}>Number of Transaction - {item?.numberOfTransaction}</p>
                 <p>Log: </p>
                 <div className={styles.log_ct}>
-                  {item?.history.split(',').map((sentence, index) => {
+                  {item?.history?.split(',').map((sentence, index) => {
                     return (
                       <p key={index} className={styles.history_sc}>
                           {index+1}. {sentence}
